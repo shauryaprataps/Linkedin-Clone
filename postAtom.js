728195f34fee45e2a58117882469a966b0de2b11
shauryaprataps <shauryaprataps1303@gmail.com>
2025-07-19 22:55:54 +0530

    Add files via upload

---
+++ postAtom.js
@@ -0,0 +1,21 @@
+import { atom } from "recoil";
+
+export const handlePostState = atom({
+  key: "handlePostState",
+  default: false,
+});
+
+export const getPostState = atom({
+  key: "getPostState",
+  default: {},
+});
+
+export const useSSRPostsState = atom({
+  key: "useSSRPostsState",
+  default: true,
+});
+
+export const selectedMediaState = atom({
+  key: "selectedMediaState",
+  default: null,
+});
